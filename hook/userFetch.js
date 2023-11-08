a729a65aa007a64ba5def75434e0027e72bb00f2
Yves IRADUKUNDA <iradukundayves11@gmail.com>
2023-11-08 19:43:35 +0200

    initial committ

--- hook/userFetch.js
+++ hook/userFetch.js
@@ -49,7 +49,7 @@ export const useFetch = (endpoint, query) => {
         } finally {
             SetIsLoading(false)
         }
-        jjjjjjjjjjjjjjjjj
+        jjjjjjjjjjjjjjjj
     useEffect(() => {
         FetchData();
     }, []);
