35217889ef4af9bf15c91058cb7d434612b82f07
Yves IRADUKUNDA <iradukundayves11@gmail.com>
2024-01-23 23:54:36 +0200

    fix: fixxing bug

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -52,7 +52,7 @@ export const Welcome = ({searchTerm,SetSearchTerm,handleclick}) => {
           }}
           >
 
-            jhjvghbjkle
+            jhjvghbjkl
             <Text style={styles.tabText(activeJobType,item)}>{item}</Text>
           </TouchableOpacity>
          )}
